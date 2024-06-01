902ddb4aca4859d627f97d9febc1ff606d805927
unknown <erick.ishimine@gmail.com>
2024-06-01 09:47:23 -0300

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# whatsapp-web
+# Whatsapp-web
+## Índice
+- [Descrição](#descrição)
+- [Tech Stack](#tech-stack)
+- [Rodando o projeto](#rodando-o-projeto)
+
+## Descrição
+App para enviar mensagens para vários grupos de whatsapp de uma só vez
+
+## Tech Stack
+- Express
+- Whatsapp-web.js
+
+## Rodando o projeto
+Instalando as dependências
+```bash
+npm install
+```
+
+Iniciando a aplicação
+```bash
+npm run app
+```
+
+No terminal irá aparecer um QR Code para fazer a autenticação do WhatsApp. Escaneie o QR Code e a aplicação será inicializada.
+
---
+++ src/app.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const whatsapp = require('./helpers/whatsapp');
+
+// Workaround - wweb 2.3*** version is not working
+const wwebVersion = '2.2412.54';
+const whatsappClient = new Client({
+    puppeteer: { headless: true },
+    authStrategy: new LocalAuth(),
+    webVersionCache: {
+      type: 'remote',
+      remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
+  },
+});
+const app = express();
+const port = 3000;
+
+whatsappClient.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+whatsappClient.on('ready', async () => {
+  app.listen(port, () => {
+    console.log(`Local:   http://localhost:${port}`);
+  })
+});
+
+whatsappClient.initialize();
+
+// Parse application/json
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.post('/send-messages', async (req, res) => {
+  const groups = req.body.groups?.split(';')?.filter((element) => element.trim().length > 0);
+  const message = req.body.message;
+
+  if (groups == null || groups.length === 0) {
+    return res.status(400).json({ message: 'Campo "groups" não pode ser vazio' });
+  }
+
+  if (message == null || message.trim().length === 0 || message.trim() == 'eita') {
+    return res.status(400).json({ message: 'Campo "message" não pode ser vazio' });
+  }
+
+  let totalErrors = 0;
+  const errors = [];
+
+  for (let group of groups) {
+    const error = await whatsapp.sendMessage(whatsappClient, group, message);
+
+    if (error) {
+      errors.push(`Grupo ${group}: ${error}`);
+      totalErrors += 1;
+    }
+  }
+
+  const total = groups.length;
+  const totalSent = total - totalErrors;
+
+  return res.status(200).json({ errors, total: groups.length, totalSent, totalErrors });
+})
